1e037866cba3ab7d994817ad410d522c87af4e67
kleanthis <spiti99@hotmail.com>
2022-04-15 00:42:00 +0100

    Adding main project file.

---
+++ satah.py
@@ -0,0 +1,13 @@
+'''
+Created on 15 Apr 2022
+
+@author: kltsa
+'''
+import sys
+from gui import create_user_interface
+
+def main():
+    create_user_interface.show()
+
+if __name__ == '__main__':
+    main()
